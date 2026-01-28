0e30abd25955095aee6bfaee3efacb2ec79b0b85
Taylor <taylorhorta@gmail.com>
2026-01-28 12:01:48 -0300

    correcao links

--- trabalhofront/vite.config.js
+++ trabalhofront/vite.config.js
@@ -5,10 +5,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        // A entrada principal sempre deve ser o index na raiz
         main: resolve(__dirname, 'index.html'),
-        // Suas páginas dentro de src/pages/
-        // Substitua 'nome_da_pagina' pelo nome real dos seus arquivos .html
         pagina1: resolve(__dirname, 'src/pages/home.html'),
         pagina2: resolve(__dirname, 'src/pages/login.html'),
       },
